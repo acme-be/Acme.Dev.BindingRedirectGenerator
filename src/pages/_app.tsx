213e543294c260af5a1bc279c4232749fb75cab0
Simon Baudart <s.baudart@wavenet.be>
2021-02-23 11:32:22 +0100

    Base form !

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import type { AppProps /*, AppContext */ } from 'next/app';
+import Head from 'next/head';
+
+export default function Application({Component, pageProps}: AppProps)
+{
+    return <>
+        <Head>
+            <title>Home Assistant PADD</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <meta name="mobile-web-app-capable" content="yes"/>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossOrigin="anonymous" />
+        </Head>
+        <Component {...pageProps} />
+    </>;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,55 @@
+import { useState } from 'react';
+
 export default function Home()
 {
-    return <div>Home</div>
+    const [ideErrorMessage, setIdeErrorMessage] = useState('');
+    const [generatedBinding, setGeneratedBinding] = useState('');
+
+    function generateBinding()
+    {
+        setGeneratedBinding(ideErrorMessage);
+    }
+
+    return <div className="container">
+        <div className="row">
+            <div className="col">
+                <h1>
+                    Binding Redirect Generator !
+                </h1>
+                <i>
+                    Because bindings are important ... but annoying !
+                </i>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col">
+                <h2>Step 1 : Input the error message from your IDE</h2>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col">
+                <textarea className="form-control" rows={15} value={ideErrorMessage} onChange={(e) => setIdeErrorMessage(e.currentTarget.value)}/>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col">
+                <button type="button" className="btn btn-primary mt-3 w-100" onClick={() => generateBinding()}>Step 2 : Click here !</button>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col mt-3">
+                <h2>Step 3 : Copy the content below and paste into your .config</h2>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col">
+                <textarea className="form-control mt3" rows={15} value={generatedBinding} readOnly={true}/>
+            </div>
+        </div>
+    </div>
 }
