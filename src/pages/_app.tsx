a36b0d34e6e7d27d5a03cb120fa9f30be939c755
Simon Baudart <s.baudart@wavenet.be>
2021-02-23 12:47:24 +0100

    Add some customization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,10 @@ export default function Application({Component, pageProps}: AppProps)
 {
     return <>
         <Head>
-            <title>Home Assistant PADD</title>
+            <title>ACME - Binding Redirects Generator</title>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <meta name="mobile-web-app-capable" content="yes"/>
+            <link rel="icon" href="/logo.png" />
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossOrigin="anonymous" />
         </Head>
         <Component {...pageProps} />
